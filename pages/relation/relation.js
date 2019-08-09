a8f592a4e0c27f4d1df8ba5557e29b1e7bcbe33c
minshenghui <1013525657@qq.com>
2019-08-09 23:43:26 +0800

    增加了地图组件

--- pages/relation/relation.js
+++ pages/relation/relation.js
@@ -5,7 +5,16 @@ Page({
    * 页面的初始数据
    */
   data: {
+    latitude: 27.6992700000,  // 经度
+    longitude: 111.4248900000,  // 纬度
 
+    markers: [{
+      id: 1,
+      latitude: 27.6992700000,
+      longitude: 111.4248900000,
+      name: '冷水江市安达驾校',
+      iconPath: '/images/my.png'
+    }],
   },
 
   /**
@@ -13,14 +22,14 @@ Page({
    */
   onLoad: function(options) {
 
-  }, 
-  calling: function () {
+  },
+  calling: function() {
     wx.makePhoneCall({
       phoneNumber: '18873882118', //此号码并非真实电话号码，仅用于测试
-      success: function () {
+      success: function() {
         console.log("拨打电话成功！")
       },
-      fail: function () {
+      fail: function() {
         console.log("拨打电话失败！")
       }
     })
